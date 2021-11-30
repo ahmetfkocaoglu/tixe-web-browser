337ce3b29250685647f267ddc309a71a3dbf08c0
ahmetfkocaoglu <82767775+ahmetfkocaoglu@users.noreply.github.com>
2021-11-30 11:10:58 +0300

    Add files via upload

---
+++ TIXEBrowser/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace TIXEBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.webToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yerİşaretlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.googleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.commodoreGenTRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.metroİstanbulToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.puyoNexusWikiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vehiclePictureDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ınternetArchiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bulbaPediaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.youTubeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sabahGazetesiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stackOverflowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.superMarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.videoGameMusicDowloadsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teletextStuffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smashWikiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(-2, 75);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(790, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(5, 102);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(790, 407);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.webToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // webToolStripMenuItem
+            // 
+            this.webToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yerİşaretlerToolStripMenuItem});
+            this.webToolStripMenuItem.Name = "webToolStripMenuItem";
+            this.webToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.webToolStripMenuItem.Text = "Web";
+            // 
+            // yerİşaretlerToolStripMenuItem
+            // 
+            this.yerİşaretlerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.googleToolStripMenuItem,
+            this.commodoreGenTRToolStripMenuItem,
+            this.metroİstanbulToolStripMenuItem,
+            this.puyoNexusWikiToolStripMenuItem,
+            this.vehiclePictureDatabaseToolStripMenuItem,
+            this.ınternetArchiveToolStripMenuItem,
+            this.bulbaPediaToolStripMenuItem,
+            this.youTubeToolStripMenuItem,
+            this.sabahGazetesiToolStripMenuItem,
+            this.stackOverflowToolStripMenuItem,
+            this.superMarToolStripMenuItem,
+            this.videoGameMusicDowloadsToolStripMenuItem,
+            this.teletextStuffToolStripMenuItem,
+            this.smashWikiToolStripMenuItem});
+            this.yerİşaretlerToolStripMenuItem.Name = "yerİşaretlerToolStripMenuItem";
+            this.yerİşaretlerToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.yerİşaretlerToolStripMenuItem.Text = "Yer İşaretleri";
+            // 
+            // googleToolStripMenuItem
+            // 
+            this.googleToolStripMenuItem.Name = "googleToolStripMenuItem";
+            this.googleToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.googleToolStripMenuItem.Text = "Google";
+            this.googleToolStripMenuItem.Click += new System.EventHandler(this.googleToolStripMenuItem_Click);
+            // 
+            // commodoreGenTRToolStripMenuItem
+            // 
+            this.commodoreGenTRToolStripMenuItem.Name = "commodoreGenTRToolStripMenuItem";
+            this.commodoreGenTRToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.commodoreGenTRToolStripMenuItem.Text = "CommodoreGenTR";
+            this.commodoreGenTRToolStripMenuItem.Click += new System.EventHandler(this.commodoreGenTRToolStripMenuItem_Click);
+            // 
+            // metroİstanbulToolStripMenuItem
+            // 
+            this.metroİstanbulToolStripMenuItem.Name = "metroİstanbulToolStripMenuItem";
+            this.metroİstanbulToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.metroİstanbulToolStripMenuItem.Text = "Metro İstanbul";
+            this.metroİstanbulToolStripMenuItem.Click += new System.EventHandler(this.metroİstanbulToolStripMenuItem_Click);
+            // 
+            // puyoNexusWikiToolStripMenuItem
+            // 
+            this.puyoNexusWikiToolStripMenuItem.Name = "puyoNexusWikiToolStripMenuItem";
+            this.puyoNexusWikiToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.puyoNexusWikiToolStripMenuItem.Text = "Puyo Nexus Wiki";
+            this.puyoNexusWikiToolStripMenuItem.Click += new System.EventHandler(this.puyoNexusWikiToolStripMenuItem_Click);
+            // 
+            // vehiclePictureDatabaseToolStripMenuItem
+            // 
+            this.vehiclePictureDatabaseToolStripMenuItem.Name = "vehiclePictureDatabaseToolStripMenuItem";
+            this.vehiclePictureDatabaseToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.vehiclePictureDatabaseToolStripMenuItem.Text = "Vehicle Picture Database";
+            this.vehiclePictureDatabaseToolStripMenuItem.Click += new System.EventHandler(this.vehiclePictureDatabaseToolStripMenuItem_Click);
+            // 
+            // ınternetArchiveToolStripMenuItem
+            // 
+            this.ınternetArchiveToolStripMenuItem.Name = "ınternetArchiveToolStripMenuItem";
+            this.ınternetArchiveToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.ınternetArchiveToolStripMenuItem.Text = "Internet Archive";
+            this.ınternetArchiveToolStripMenuItem.Click += new System.EventHandler(this.ınternetArchiveToolStripMenuItem_Click);
+            // 
+            // bulbaPediaToolStripMenuItem
+            // 
+            this.bulbaPediaToolStripMenuItem.Name = "bulbaPediaToolStripMenuItem";
+            this.bulbaPediaToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.bulbaPediaToolStripMenuItem.Text = "BulbaPedia";
+            this.bulbaPediaToolStripMenuItem.Click += new System.EventHandler(this.bulbaPediaToolStripMenuItem_Click);
+            // 
+            // youTubeToolStripMenuItem
+            // 
+            this.youTubeToolStripMenuItem.Name = "youTubeToolStripMenuItem";
+            this.youTubeToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.youTubeToolStripMenuItem.Text = "YouTube";
+            this.youTubeToolStripMenuItem.Click += new System.EventHandler(this.youTubeToolStripMenuItem_Click);
+            // 
+            // sabahGazetesiToolStripMenuItem
+            // 
+            this.sabahGazetesiToolStripMenuItem.Name = "sabahGazetesiToolStripMenuItem";
+            this.sabahGazetesiToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.sabahGazetesiToolStripMenuItem.Text = "Sabah Gazetesi";
+            this.sabahGazetesiToolStripMenuItem.Click += new System.EventHandler(this.sabahGazetesiToolStripMenuItem_Click);
+            // 
+            // stackOverflowToolStripMenuItem
+            // 
+            this.stackOverflowToolStripMenuItem.Name = "stackOverflowToolStripMenuItem";
+            this.stackOverflowToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.stackOverflowToolStripMenuItem.Text = "StackOverflow";
+            this.stackOverflowToolStripMenuItem.Click += new System.EventHandler(this.stackOverflowToolStripMenuItem_Click);
+            // 
+            // superMarToolStripMenuItem
+            // 
+            this.superMarToolStripMenuItem.Name = "superMarToolStripMenuItem";
+            this.superMarToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.superMarToolStripMenuItem.Text = "Super Mario Wiki";
+            this.superMarToolStripMenuItem.Click += new System.EventHandler(this.superMarToolStripMenuItem_Click);
+            // 
+            // videoGameMusicDowloadsToolStripMenuItem
+            // 
+            this.videoGameMusicDowloadsToolStripMenuItem.Name = "videoGameMusicDowloadsToolStripMenuItem";
+            this.videoGameMusicDowloadsToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.videoGameMusicDowloadsToolStripMenuItem.Text = "Video Game Music Dowloads";
+            this.videoGameMusicDowloadsToolStripMenuItem.Click += new System.EventHandler(this.videoGameMusicDowloadsToolStripMenuItem_Click);
+            // 
+            // teletextStuffToolStripMenuItem
+            // 
+            this.teletextStuffToolStripMenuItem.Name = "teletextStuffToolStripMenuItem";
+            this.teletextStuffToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.teletextStuffToolStripMenuItem.Text = "Teletext Stuff";
+            this.teletextStuffToolStripMenuItem.Click += new System.EventHandler(this.teletextStuffToolStripMenuItem_Click);
+            // 
+            // smashWikiToolStripMenuItem
+            // 
+            this.smashWikiToolStripMenuItem.Name = "smashWikiToolStripMenuItem";
+            this.smashWikiToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+            this.smashWikiToolStripMenuItem.Text = "SmashWiki";
+            this.smashWikiToolStripMenuItem.Click += new System.EventHandler(this.smashWikiToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(127, 37);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(32, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "F5";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(165, 37);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(51, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "HOME";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(222, 39);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(72, 19);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "ARA";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 521);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem webToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yerİşaretlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem googleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem commodoreGenTRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem metroİstanbulToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem puyoNexusWikiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vehiclePictureDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ınternetArchiveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bulbaPediaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem youTubeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sabahGazetesiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stackOverflowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem superMarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem videoGameMusicDowloadsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teletextStuffToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ToolStripMenuItem smashWikiToolStripMenuItem;
+    }
+}
+
---
+++ TIXEBrowser/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIXEBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void googleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://www.google.com.tr");
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack == true && webBrowser1.IsOffline == false)
+            {
+                webBrowser1.GoBack();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward == true && webBrowser1.IsOffline == false)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsBusy == true && webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Stop();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsBusy == false && webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Refresh();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.GoHome();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.GoSearch();
+            }
+        }
+
+        private void commodoreGenTRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("http://www.commodore.gen.tr");
+            }
+        }
+
+        private void metroİstanbulToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://www.metro.istanbul");
+            }
+        }
+
+        private void puyoNexusWikiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://puyonexus.com");
+            }
+        }
+
+        private void vehiclePictureDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("http://pc2.pxtr.de");
+            }
+        }
+
+        private void ınternetArchiveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://archive.org");
+            }
+        }
+
+        private void bulbaPediaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://bulbapedia.bulbagarden.net");
+            }
+        }
+
+        private void youTubeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://www.youtube.com");
+            }
+        }
+
+        private void sabahGazetesiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("http://www.sabah.com.tr");
+            }
+        }
+
+        private void stackOverflowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("http://stackoverflow.com");
+            }
+        }
+
+        private void superMarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://www.mariowiki.com");
+            }
+        }
+
+        private void videoGameMusicDowloadsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://downloads.khinsider.com");
+            }
+        }
+
+        private void teletextStuffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://zxnet.co.uk/teletext");
+            }
+        }
+
+        private void smashWikiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.IsOffline == false)
+            {
+                webBrowser1.Navigate("https://www.ssbwiki.com");
+            }
+        }
+    }
+}
